9b24e265d9e1f593f54885b393d07c1ca95a75c3
MARIO <aboodhu0@gmail.com>
2023-06-17 02:35:19 -0700

    mario2 update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Mario is The Best</h1>
+        <h2>Mario is The Best</h2>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
