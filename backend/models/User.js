4c21d60983b284e918e342c7ead796d72c13659f
laythKH <laythkhoury.101@gmail.com>
2023-06-22 21:07:33 +0300

    commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,19 +5,6 @@ import jwt from 'jsonwebtoken';
 
 
 const UserSchema = new mongoose.Schema({
-   // firstName: {
-   //    type: String,
-   //    required: [true, 'Please provide name'],
-   //    minlength: 3,
-   //    maxlength: 20,
-   //    trim: true,
-   // },
-   // lastName: {
-   //    type: String,
-   //    minlength: 3,
-   //    maxlength: 20,
-   //    trim: true,
-   // },
    name: {
       type: String,
       minlength: 5,
